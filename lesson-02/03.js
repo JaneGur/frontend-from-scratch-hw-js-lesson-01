f0f2d7f245fd21c5f12ce1f325af7a5184274c92
Evgeniya <iliving2007@yandex.ru>
2025-04-05 11:21:27 +0300

    Task2.2

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,35 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
-let grade
-// your code
+const score = 70; // тестовое значение, можно изменять
+let grade;
+let isgradeA = score >= 90 && score <= 100;
+let isgradeB = score >= 80 && score <= 89;
+let isgradeC = score >= 70 && score <= 79;
+let isgradeD = score >= 50 && score <= 69;
+let isgradeF = score >= 0 && score <= 49;
+
+console.log(isgradeA);
+console.log(isgradeB);
+console.log(isgradeC);
+console.log(isgradeD);
+console.log(isgradeF);
+
+if (isgradeA) {
+  grade = "A";
+  alert(`Ваша оценка ${grade}`);
+} else if (isgradeB) {
+  grade = "B";
+  alert(`Ваша оценка ${grade}`);
+} else if (isgradeC) {
+  grade = "C";
+  alert(`Ваша оценка ${grade}`);
+} else if (isgradeD) {
+  grade = "D";
+  alert(`Ваша оценка ${grade}`);
+} else if (isgradeF) {
+  grade = "F";
+  alert(`Ваша оценка ${grade}`);
+} else {
+  alert("Ни одно из значений");
+}
